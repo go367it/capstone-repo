c000dbcb56e649c56776699324023454df8e231e
go367it <satyamkumar3001@gmail.com>
2023-05-12 00:50:57 +0530

    ajns

--- src/pages/teacher/Home/TeacherHome.js
+++ src/pages/teacher/Home/TeacherHome.js
@@ -1,57 +1,26 @@
 import * as React from "react";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 
 export default function TeacherHome() {
   return (
     <>
-      <Stack sx={{ width: "100%", textAlign: "left" }}>
-        <Alert sx={{ width: "100%" }} severity="info">
-          <AlertTitle sx={{ textAlign: "left" }}>Information</AlertTitle>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur
-        </Alert>
-      </Stack>
-
-      <Box sx={{ flexGrow: 1, marginTop: "1rem" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <Card variant="outlined" sx={{ maxWidth: 345 }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Tests
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-          
-        </Grid>
-      </Box>
+      <div className="w-full p-4">
+        <div className="message space-y-2 w-full rounded-lg text-blue-800 p-4 bg-blue-100">
+          <span className="flex space-x-4 place-items-center">
+            <InfoOutlinedIcon />
+            <p className=" text-lg font-medium">Information</p>
+          </span>
+          <div className=" ml-10">
+            <p>
+              Attention! New feature will coming to the platform so get excited
+            </p>
+            <p>
+              Some of the new features we are working on are: Instant connect
+              between users, more security etc..
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
