4bd99787bb6c6f2bb721a1fdf50626b65d1ace60
go367it <satyamkumar3001@gmail.com>
2023-05-11 23:57:55 +0530

    added tailwind css

--- src/pages/login-signup/Login.js
+++ src/pages/login-signup/Login.js
@@ -1,18 +1,17 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import { Link } from "react-router-dom";
 import { useContext, useState } from "react";
 import MessageContext from "../../context/MessageContext";
 import { useNavigate } from "react-router-dom";
+import Zoom from "react-reveal/Zoom";
 
-// firebase import 
+// firebase import
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../FirebaseApp";
-
 
 export default function Login() {
   const { setOpen, setType, setMessage } = useContext(MessageContext);
@@ -50,83 +49,91 @@ export default function Login() {
   const navigate = useNavigate();
   const handleSubmit = () => {
     if (filedChecking() == true) {
-
       // user login
       signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential)=>{
-        console.log(userCredential)
-        setOpen(true);
+        .then((userCredential) => {
+          console.log(userCredential);
+          setOpen(true);
           setType("success");
           setMessage("Login Successfull!");
           navigate("/teacher/home");
-      })
-      .catch((error)=>{
-        setOpen(true);
+        })
+        .catch((error) => {
+          setOpen(true);
           setType("error");
           setMessage(`${error.code}!`);
-        console.log(error.message, error.code)
-      })
-
+          console.log(error.message, error.code);
+        });
     }
   };
 
   return (
     <Container sx={{ mt: 3 }} maxWidth="sm">
-      <Card variant="outlined" sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Stack>
-            <div className="d-flex justify-content-center">
-              <h4>Login</h4>
-            </div>
-            <div className="mt-3">
-              <label for="email" className="form-label">
-                Email address
-              </label>
-              <input
-                value={email}
-                onChange={(e) => onChange(e)}
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="name@example.com"
-              />
-            </div>
-            <div className="mt-3">
-              <label for="password" className="form-label">
-                Password
-              </label>
-              <input
-                value={password}
-                onChange={(e) => onChange(e)}
-                type="password"
-                className="form-control"
-                id="password"
-              />
-            </div>
-            <div className="mt-3 d-flex justify-content-center">
-              <span>
-                Don't have an Account?
-                <Link style={{ textDecoration: "none" }} to="/signup">
-                  {" "}
-                  Sign Up!
-                </Link>
-              </span>
-            </div>
+      <Zoom>
+        <Card variant="outlined" sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Stack>
+              <div className="flex justify-center text-center w-full text-3xl font-semibold">
+                <h4>Login</h4>
+              </div>
+              <div className="mt-3">
+                <div for="email" className="form-label">
+                  Email address
+                </div>
+                <input
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                  type="email"
+                  className="mt-3 w-full px-3 py-2 border border-gray-300 rounded-lg outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform duration-300
+                "
+                  id="email"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div className="mt-3">
+                <div for="password" className="form-label">
+                  Password
+                </div>
+                <input
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                  type="password"
+                  className="mt-3 w-full px-3 py-2 border border-gray-300 rounded-lg outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform duration-300
+                "
+                  id="password"
+                />
+              </div>
+              <div className="mt-3 text-center">
+                <span>
+                  Don't have an Account?
+                  <Link
+                    className=" text-blue-500"
+                    style={{ textDecoration: "none" }}
+                    to="/signup"
+                  >
+                    {" "}
+                    Sign Up!
+                  </Link>
+                </span>
+              </div>
 
-            <div className="mt-3 d-flex justify-content-center">
-              <Button
-                onClick={() => handleSubmit()}
-                variant="contained"
-                disableElevation
-                color="primary"
-                size="medium"
-              >
-                Login
-              </Button>
-            </div>
-          </Stack>
-        </CardContent>
-      </Card>
+              <div className="mt-3 flex justify-center">
+                <button
+                  className="mt-3 px-3 py-2 rounded-lg bg-indigo-600 text-white w-1/3
+              hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+              transform duration-300
+              "
+                  onClick={() => handleSubmit()}
+                >
+                  Login
+                </button>
+              </div>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Zoom>
     </Container>
   );
 }
--- src/pages/login-signup/SIgnUp.js
+++ src/pages/login-signup/SIgnUp.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import MessageContext from "../../context/MessageContext";
 import { useNavigate } from "react-router-dom";
+import Zoom from "react-reveal/Zoom";
 
 // firebase import
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -60,7 +61,6 @@ const SIgnUp = () => {
   const navigate = useNavigate();
   // function for handling submit button
   const handleSubmit = () => {
-    
     if (filedChecking() == true) {
       // creating user
       createUserWithEmailAndPassword(auth, email, password)
@@ -69,36 +69,31 @@ const SIgnUp = () => {
           setOpen(true);
           setType("success");
           setMessage("User Registered!");
-          
-          
-          setTimeout(()=>{
+
+          setTimeout(() => {
             navigate("/");
-          }, 3000)
+          }, 3000);
         })
         .catch((error) => {
-          
           // displaying error message
-          if(error.code == "auth/email-already-in-use"){
+          if (error.code == "auth/email-already-in-use") {
             setOpen(true);
             setType("error");
             setMessage("Email Already Exists!");
-          }
-          else if(error.code == "auth/invalid-email"){
+          } else if (error.code == "auth/invalid-email") {
             setOpen(true);
             setType("error");
             setMessage("Invalid Email!");
-          }
-          else if(error.code == "auth/weak-password"){
+          } else if (error.code == "auth/weak-password") {
             setOpen(true);
             setType("error");
             setMessage("Weak Password!");
-          }
-          else{
+          } else {
             setOpen(true);
             setType("error");
             setMessage(`${error.code}`);
           }
-          
+
           console.log(error.message, error.code);
           console.log(error);
         });
@@ -107,73 +102,85 @@ const SIgnUp = () => {
 
   return (
     <Container sx={{ mt: 3 }} maxWidth="sm">
-      <Card variant="outlined" sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Stack>
-            <div className="d-flex justify-content-center">
-              <h4>Sign Up</h4>
-            </div>
-            <div className="mt-3">
-              <label for="email" className="form-label">
-                Email address
-              </label>
-              <input
-                value={email}
-                onChange={(e) => onChange(e)}
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="name@example.com"
-              />
-            </div>
-            <div className="mt-3">
-              <label for="password" className="form-label">
-                Password
-              </label>
-              <input
-                value={password}
-                onChange={(e) => onChange(e)}
-                type="password"
-                className="form-control"
-                id="password"
-              />
-            </div>
-            <div className="mt-3">
-              <label for="confirm-password" className="form-label">
-                Confirm Password
-              </label>
-              <input
-                value={confirmPassword}
-                onChange={(e) => onChange(e)}
-                type="password"
-                className="form-control"
-                id="confirm-password"
-              />
-            </div>
-            <div className="mt-3 d-flex justify-content-center">
-              <span>
-                Already have an Account?
-                <Link style={{ textDecoration: "none" }} to="/">
-                  {" "}
-                  Sign In!
-                </Link>
-              </span>
-            </div>
+      <Zoom>
+        <Card variant="outlined" sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Stack>
+              <div className="flex justify-center text-center w-full text-3xl font-semibold">
+                <h4>Sign Up</h4>
+              </div>
+              <div className="mt-3">
+                <div for="email" className="form-label">
+                  Email address
+                </div>
+                <input
+                  value={email}
+                  onChange={(e) => onChange(e)}
+                  type="email"
+                  className="mt-3 w-full px-3 py-2 border border-gray-300 rounded-lg outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform duration-300
+                "
+                  id="email"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div className="mt-3">
+                <div for="password" className="form-label">
+                  Password
+                </div>
+                <input
+                  value={password}
+                  onChange={(e) => onChange(e)}
+                  type="password"
+                  className="mt-3 w-full px-3 py-2 border border-gray-300 rounded-lg outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform duration-300
+                "
+                  id="password"
+                />
+              </div>
+              <div className="mt-3">
+                <div for="confirm-password" className="form-label">
+                  Confirm Password
+                </div>
+                <input
+                  value={confirmPassword}
+                  onChange={(e) => onChange(e)}
+                  type="password"
+                  className="mt-3 w-full px-3 py-2 border border-gray-300 rounded-lg outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform duration-300
+                "
+                  id="confirm-password"
+                />
+              </div>
+              <div className="mt-3 text-center">
+                <span>
+                  Already have an Account?
+                  <Link
+                    className=" text-blue-500"
+                    style={{ textDecoration: "none" }}
+                    to="/"
+                  >
+                    {" "}
+                    Sign In!
+                  </Link>
+                </span>
+              </div>
 
-            <div className="mt-3 d-flex justify-content-center">
-              <Button
-                onClick={() => handleSubmit()}
-                variant="contained"
-                disableElevation
-                color="primary"
-                size="medium"
-              >
-                Sign Up
-              </Button>
-            </div>
-          </Stack>
-        </CardContent>
-      </Card>
+              <div className="mt-3 flex justify-center">
+                <button
+                  className="mt-3 px-3 py-2 rounded-lg bg-indigo-600 text-white w-1/3
+              hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+              transform duration-300
+              "
+                  onClick={() => handleSubmit()}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Zoom>
     </Container>
   );
 };
